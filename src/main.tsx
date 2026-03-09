17c147352adaab54dec03521f1a86b699c2a68c8
Yosy Aliffakry <yoshiafk@f50ciety.local>
2026-03-09 15:53:47 +0700

    fix: add ActivityProvider to main React tree

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { MemberProvider } from './contexts/MemberContext.tsx'
+import { ActivityProvider } from './contexts/ActivityContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MemberProvider>
-      <App />
+      <ActivityProvider>
+        <App />
+      </ActivityProvider>
     </MemberProvider>
   </StrictMode>,
 )
